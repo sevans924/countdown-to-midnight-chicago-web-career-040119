8af1a40402442cc4610d8752f2998724a3befa08
Sarah Elzinga <sevans924@gmail.com>
2019-03-21 20:08:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,6 +4,9 @@ def countdown(seconds)
   while seconds > 0
     puts "#{seconds} SECOND(S)!"
     seconds -= 1
+    sleep(1.second)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds)
